9f2ba73c851926debb57fff2eb006cb663a5f6ee
Nelio Santos <nsfilho@icloud.com>
2022-09-21 12:03:29 -0300

    feat(core): starting project

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+extern crate rocket;
+
+use chrono::{DateTime, Utc};
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct EchoResponse {
+    unique_id: String,
+    server_time: DateTime<Utc>,
+}
+
+#[get("/")]
+fn index() -> Json<EchoResponse> {
+    let unique_id = uuid::Uuid::new_v4()
+        .hyphenated()
+        .encode_lower(&mut uuid::Uuid::encode_buffer())
+        .to_owned();
+    Json(EchoResponse {
+        unique_id,
+        server_time: Utc::now(),
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
